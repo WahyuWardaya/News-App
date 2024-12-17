13206982b1567432eb72bb9a19899d4c7b8941a8
Wahyu Wardaya <147429375+WahyuWardaya@users.noreply.github.com>
2024-12-17 18:14:33 +0800

    update navbar dan route path menghilangkan navigai home agar indonesia menjadi halaman utama website

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
 import Indonesia from './pages/Indonesia';
 import Programming from './pages/Programming';
 import Saved from './pages/Saved';
@@ -12,8 +11,7 @@ function App() {
       <Navbar />
       <div className="container mt-4">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/indonesia" element={<Indonesia />} />
+          <Route path="/" element={<Indonesia />} />
           <Route path="/programming" element={<Programming />} />
           <Route path="/saved" element={<Saved />} />
           <Route path="/search" element={<Search />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,11 +25,6 @@ function Navbar() {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/indonesia">
                 Indonesia
               </Link>
             </li>
